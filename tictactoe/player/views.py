2e125c6816b383a6906fb18c1afbb644f9b912c2
Yuzo Saito <yuzo.saito387@gmail.com>
2018-12-07 14:01:23 -0500

    add template

---
+++ tictactoe/player/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+from gameplay.models import Game
+
+def home(request):
+    games_first_player = Game.objects.filter(
+        first_player=request.user,
+        status='F'
+    )
+    games_second_player = Game.objects.filter(
+        second_player=request.user,
+        status='S'
+    )
+    all_my_games = list(games_first_player) + list(games_second_player)
+
+    return render(request, "player/home.html",
+                  {'games' : all_my_games})
