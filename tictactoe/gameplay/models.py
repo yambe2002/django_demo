17fa9d6a34e7ba19901ab4726c3a2f4e350b0671
Yuzo Saito <yuzo.saito387@gmail.com>
2018-12-07 14:24:28 -0500

    add some more

--- tictactoe/gameplay/models.py
+++ tictactoe/gameplay/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models import Q
 from django.contrib.auth.models import User
 
 GAME_STATUS_CHOICES = (
@@ -9,6 +10,17 @@ GAME_STATUS_CHOICES = (
         ('D', 'Draw')
 )
 
+class GamesQuerySet(models.QuerySet):
+    def games_for_user(self, user):
+        return self.filter(
+            Q(first_player=user) | Q(second_player=user)
+        )
+
+    def active(self):
+        return self.filter(
+            Q(status='F') | Q(status='S')
+        )
+
 
 class Game(models.Model):
     first_player = models.ForeignKey(User,
@@ -18,6 +30,8 @@ class Game(models.Model):
     start_time = models.DateTimeField(auto_now_add=True)
     last_time = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=1, default='F', choices=GAME_STATUS_CHOICES)
+    
+    objects = GamesQuerySet.as_manager()
 
     def __str__(self):
         return "{0} vs {1}".format(
--- tictactoe/player/views.py
+++ tictactoe/player/views.py
@@ -3,15 +3,8 @@ from django.shortcuts import render
 from gameplay.models import Game
 
 def home(request):
-    games_first_player = Game.objects.filter(
-        first_player=request.user,
-        status='F'
-    )
-    games_second_player = Game.objects.filter(
-        second_player=request.user,
-        status='S'
-    )
-    all_my_games = list(games_first_player) + list(games_second_player)
+    my_games = Game.objects.games_for_user(request.user)
+    active_games = my_games.active()
 
     return render(request, "player/home.html",
-                  {'games' : all_my_games})
+                  {'games' : active_games})
