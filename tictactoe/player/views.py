51a306100ceb938160310bc36609b46d5290d76e
Yuzo Saito <yuzo.saito387@gmail.com>
2018-12-07 16:23:25 -0500

    add auth

--- tictactoe/player/views.py
+++ tictactoe/player/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
 
 from gameplay.models import Game
 
+@login_required
 def home(request):
     my_games = Game.objects.games_for_user(request.user)
     active_games = my_games.active()
